6644b2797ac18c1da3b1ebe6d04d239a0f4bc6a7
yeeeppppp <gulslinegg@gmail.com>
2025-03-21 02:05:52 +0300

    оур театчерс доделал

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import './App.css';
 import Sosal from './components/sosal/sosal'
+import FormatLearning from './components/FormatLearning/FormatLearning';
+import OurTeachers from './components/OurTeachers/ourteachers';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Sosal" element={<Sosal />} />
+          <Route path='/OurTeachers' element={<OurTeachers />}/>
         </Routes>
       </div>
     </Router>
