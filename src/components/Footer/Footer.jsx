bc5bc42f18e60ea5441943d99bb6c825ac0bd666
ivwdie <ivwdie@icloud.com>
2025-03-23 13:17:46 +0300

    SEMIPREFINAL

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,31 +24,32 @@ function Footer(){
             </footer>
             <div className="links-to-docs">
                 <div className="group-links">
-                    <a href="">Основные сведения</a>
-                    <a href="">Структура и органы управления образовательной организацией</a>
-                    <a href="">Документ об утверждении стоимости обучения</a>
-                    <a href="">Образец договора об оказании платных образовательных услуг</a>
+                    <a target='_blank' href="https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5%20%D0%A1%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F.pdf&name=%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5%20%D0%A1%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F.pdf&nosw=1">Основные сведения</a>
+                    <a target='_blank' href="">Структура и органы управления образовательной организацией</a>
+                    <a target='_blank' href="">Документ об утверждении стоимости обучения</a>
+                    <a target='_blank' href="">Образец договора об оказании платных образовательных услуг</a>
                 </div>
                 <div className="group-links">
-                    <a href="">Образование</a>
-                    <a href="">Руководство и педагогический состав</a>
-                    <a href="">Материально-техническое обучение и оснащенность образовательного процессая</a>
-                    <a href="">Правила приема обучающихся</a>
-                    <a href="">Устав</a>
+                    <a target='_blank' href="">Образование</a>
+                    <a target='_blank' href="">Руководство и педагогический состав</a>
+                    <a target='_blank' href="">Материально-техническое обучение и оснащенность образовательного процессая</a>
+                    <a target='_blank' href="">Правила приема обучающихся</a>
+                    <a target='_blank' href="https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%A3%D1%81%D1%82%D0%B0%D0%B2.pdf&name=%D0%A3%D1%81%D1%82%D0%B0%D0%B2.pdf&nosw=1">Устав</a>
                 </div>
                 <div className="group-links">
-                    <a href="">Платные образовательные услуги</a>
-                    <a href="">Финансово-хозяйственная деятельность</a>
-                    <a href="">Вакантные места для приема (перевода) обучающихся</a>
-                    <a href="">Положение о режиме занятий обучающихся</a>
+                    <a target='_blank' href="">Платные образовательные услуги</a>
+                    <a target='_blank' href="">Финансово-хозяйственная деятельность</a>
+                    <a target='_blank' href="">Вакантные места для приема (перевода) обучающихся</a>
+                    <a target='_blank' href="">Положение о режиме занятий обучающихся</a>
                     
                 </div>
                 <div className="group-links">
-                    <a href="">Доступная среда</a>
-                    <a href="">Международное сотрудничество</a>
-                    <a href="">Акт самообследования</a>
-                    <a href="">Реквизиты</a>
-                    <a href="">Документы</a>
+                    <a target='_blank' href="">Доступная среда</a>
+                    <a target='_blank' href="">Международное сотрудничество</a>
+                    <a target='_blank' href="">Акт самообследования</a>
+                    <a target='_blank' href="https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2Fe96PCbs%2F0wWCNOEBWwRHAuvIICmRcqjQTS7gOhVLtOwpNCL2aYtTji3PQGTGeU%2Fcq%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2F%D0%A0%D0%B5%D0%BA%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D1%8B.pdf&name=%D0%A0%D0%B5%D0%BA%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D1%8B.pdf&nosw=1">Реквизиты</a>
+                    <a target='_blank' href="">Документы</a>
+                    
                 </div>
             </div>
             <div className="copyright">
@@ -57,5 +58,7 @@ function Footer(){
         </>
     )
 }
+
+// negrashl@mail.ru
 
 export default Footer;
